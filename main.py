79d96ad44471d849bccd5fb317c7bd4a86ab5c43
Anshu Sinha <anshujuly2@gmail.com>
2023-04-30 23:11:17 +0530

    Add validation to email and password fields

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from flask_wtf import FlaskForm
 # imported StringField class from wtforms module
 from wtforms import StringField, PasswordField, SubmitField
 
+# imported DataRequired class from wtforms.validators module
+from wtforms.validators import DataRequired, Email, Length
 
 # created an instance of the Flask class and stored it in a variable called app
 app = Flask(__name__)
@@ -16,19 +18,22 @@ app.secret_key = 'dfvwejnilo3442-hjguuiuygl32r23!@#'
 
 """class to create a form with a username and password field"""
 class Login_Form(FlaskForm):
-    email = StringField(label='Email')
-    password = PasswordField(label='Password')
-    submit = SubmitField(label='Log In')
+    email = StringField(label='Email', validators=[DataRequired(), Email('Invalid email address!')])
+    password = PasswordField(label='Password', validators=[DataRequired(), Length(min=8, max=20)])
+    submit=SubmitField(label='Log In')
 
 # created a route decorator to tell Flask what URL should trigger our function
-@app.route("/")
+@ app.route("/")
 def home():
     return render_template('index.html')
 
-@app.route("/login")
+# created a route decorator to tell Flask what URL should trigger our function
+@ app.route("/login", methods=['GET', 'POST'])
 def login():
-    login_form = Login_Form()
+    login_form=Login_Form()
+    login_form.validate_on_submit()
     return render_template('login.html', form=login_form)
+
 
 # check if the executed file is the main program and run the app
 if __name__ == '__main__':
