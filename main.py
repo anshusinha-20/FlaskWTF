bc55df560341fb7f5dc49ac5b4ac277fe9d5bce8
Anshu Sinha <anshujuly2@gmail.com>
2023-04-30 21:51:51 +0530

    Add a code to create forms with flask-wtf, obscured fields like possword

---
+++ main.py
@@ -0,0 +1,34 @@
+# imported Flask and render_template class from flask module
+from flask import Flask, render_template
+
+# imported FlaskForm class from flask_wtf module
+from flask_wtf import FlaskForm
+
+# imported StringField class from wtforms module
+from wtforms import StringField, PasswordField
+
+
+# created an instance of the Flask class and stored it in a variable called app
+app = Flask(__name__)
+
+# variable to store the secret key
+app.secret_key = 'dfvwejnilo3442-hjguuiuygl32r23!@#'
+
+"""class to create a form with a username and password field"""
+class Login_Form(FlaskForm):
+    email = StringField('Email')
+    password = PasswordField('Password')
+
+# created a route decorator to tell Flask what URL should trigger our function
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+@app.route("/login")
+def login():
+    login_form = Login_Form()
+    return render_template('login.html', form=login_form)
+
+# check if the executed file is the main program and run the app
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5001)
